d865eacb676bddef1da0b0bec98337238e3f29a5
unknown <BT_4N2_15@BKTECH.LOCAL>
2023-03-28 11:57:36 -0400

    Archived U9L7 exploration
    Polymorphism

---
+++ src/Entertainer.java
@@ -0,0 +1,15 @@
+public class Entertainer {
+    private String talent;
+
+    public Entertainer(String t) {
+        talent = t;
+    }
+
+    public String getTalent() {
+        return talent;
+    }
+
+    public void announceTalent() {
+        System.out.println("My talent is " + talent);
+    }
+}
